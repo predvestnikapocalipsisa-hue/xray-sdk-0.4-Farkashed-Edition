8bf9d58464a395830163400b40b9d373835f3f22
Заря <predvestnikapocalipsisa@gmail.com>
2026-04-20 10:47:36 +0300

    Исправление object list
    теперь на shift + лкм можно выбирать спектр обьектов, как в оригинальном сдк. Хз почему этого нет в tsmp

--- Editors/LevelEditor/UI/UIObjectList.h
+++ Editors/LevelEditor/UI/UIObjectList.h
@@ -1,32 +1,37 @@
 #pragma once
+#include <vector> 
+
 class CCustomObject;
+
 class UIObjectList : public XrUI
 {
 public:
-	UIObjectList();
-	virtual ~UIObjectList();
-	virtual void Draw();
-	static void Update();
-	static void Show();
-	static void Close();
-	static IC bool IsOpen() { return Form; }
+    UIObjectList();
+    virtual ~UIObjectList();
+    virtual void Draw();
+    static void Update();
+    static void Show();
+    static void Close();
+    static IC bool IsOpen() { return Form; }
 
 private:
-	static UIObjectList *Form;
+    static UIObjectList* Form;
 
 private:
-	void DrawObjects();
-	void DrawObject(CCustomObject *obj, const char *name);
+    void DrawObjects();
+    void DrawObject(CCustomObject* obj, const char* name);
 
 private:
-	ObjClassID m_cur_cls;
-	enum EMode
-	{
-		M_All,
-		M_Visible,
-		M_Inbvisible
-	};
-	EMode m_Mode;
-	CCustomObject *m_SelectedObject;
-	string_path m_Filter;
+    ObjClassID m_cur_cls;
+    enum EMode
+    {
+        M_All,
+        M_Visible,
+        M_Inbvisible
+    };
+    EMode m_Mode;
+    CCustomObject* m_SelectedObject;
+    CCustomObject* m_AnchorObject;               
+    std::vector<CCustomObject*> m_VisibleRefs;   
+    string_path m_Filter;
 };
