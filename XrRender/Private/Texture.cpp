a795856e67161e5076af6bc0c1e980454bfebf55
Заря <predvestnikapocalipsisa@gmail.com>
2026-08-15 00:57:08 +0300

    Фикс бага логирования
    Теперь при отсутсвии текстуры в лог будет выводится соответствующее сообщение

--- XrRender/Private/Texture.cpp
+++ XrRender/Private/Texture.cpp
@@ -306,10 +306,8 @@ ID3DBaseTexture* CRender::texture_load(LPCSTR fRName, u32& ret_msize)
 
 
 #ifdef _EDITOR
-	if (strstr(Core.Params, "-dbg_log"))
-		ELog.Msg(mtError, "Can't find texture '%s'", fname);
+	ELog.Msg(mtError, "! Can't find texture '%s'", fname);
 #else
-	//Debug.fatal(DEBUG_INFO,"Can't find texture '%s'",fname);
 	Msg("! Can't find texture '%s'", fname);
 #endif
 	return 0;
